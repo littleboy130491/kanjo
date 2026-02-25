25e7733cb09f7643fcfe521d047a5ff20740c664
unknown <littleboy_13@yahoo.com>
2026-02-25 15:58:28 +0700

    feat(seeder): add ClientSeeder with 2 test clients

---
+++ database/seeders/ClientSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Client;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClientSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    public function run(): void
+    {
+        // Client 1: Indonesian Tech Startup
+        Client::create([
+            'name' => 'Budi Santoso',
+            'company' => 'PT Teknologi Maju',
+            'email' => 'budi@teknologimaju.co.id',
+            'phone' => '+62 812 3456 7890',
+            'notes' => [
+                [
+                    'note' => 'Prefer communication via email',
+                    'date' => '2026-01-15',
+                ],
+                [
+                    'note' => 'Budget approved for Q1 2026',
+                    'date' => '2026-01-20',
+                ],
+            ],
+        ]);
+
+        // Client 2: International E-commerce Company
+        Client::create([
+            'name' => 'Sarah Chen',
+            'company' => 'ShopGlobal Pte Ltd',
+            'email' => 'sarah.chen@shopglobal.sg',
+            'phone' => '+65 9123 4567',
+            'notes' => [
+                [
+                    'note' => 'Requires English documentation',
+                    'date' => '2026-02-01',
+                ],
+            ],
+        ]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             CompanySeeder::class,
-            ProposalSeeder::class,
+            ClientSeeder::class,
         ]);
     }
 }
