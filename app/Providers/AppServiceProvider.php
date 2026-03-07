07311417301215ad422b7ebb7d6edadef25f06a4
Henry <arisman.henry@gmail.com>
2026-03-07 14:22:56 +0700

    refactor(curator): remove temporary glide override

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,8 +5,6 @@ namespace App\Providers;
 use App\Policies\ActivityPolicy;
 use App\Policies\MediaPolicy;
 use App\Policies\RolePolicy;
-use Awcodes\Curator\Facades\Glide;
-use Awcodes\Curator\Glide\SymfonyResponseFactory;
 use Awcodes\Curator\Models\Media;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
@@ -21,15 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        Glide::serverConfig([
-            'driver' => 'imagick',
-            'response' => new SymfonyResponseFactory(app('request')),
-            'source' => storage_path('app'),
-            'source_path_prefix' => 'public',
-            'cache' => storage_path('app'),
-            'cache_path_prefix' => '.cache',
-            'max_image_size' => 2000 * 2000,
-        ]);
+        //
     }
 
     /**
