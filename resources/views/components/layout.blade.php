0345965d17472476fc3ff1dccbeaca16740b2087
Henry <littleboy13@yahoo.com>
2026-08-31 10:08:30 +0700

    fix(frontend): use single-column proposal sections

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -45,7 +45,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     @if($sansOnly)
-        <link href="https://fonts.googleapis.com/css2?family=Source+Sans+3:wght@400;500;600&family=Urbanist:wght@200;300;400;500;600&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@300;400;500;600&family=Source+Sans+3:wght@400;500;600&display=swap" rel="stylesheet">
     @else
         <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400&family=Montserrat:wght@200;300;400;500;600&display=swap" rel="stylesheet">
     @endif
